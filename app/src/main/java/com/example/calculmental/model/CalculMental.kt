1569ca60bdcd1c55e83ebc8bfcbdfc78d136420b
Lia <n@n>
2020-04-09 16:32:00 +0200

    Initialisation

---
+++ app/src/main/java/com/example/calculmental/model/CalculMental.kt
@@ -0,0 +1,12 @@
+package com.example.calculmental.model
+
+interface CalculMental {
+
+    fun nouvelleQuestion()
+
+    fun repeteLaQuestion()
+
+    fun donneMoiLeResultat() : Int
+
+    fun ecritLaQuestion() : String
+}
---
+++ app/src/main/java/com/example/calculmental/model/TheVoice.kt
@@ -0,0 +1,20 @@
+package com.example.calculmental.model
+
+class TheVoice : CalculMental {
+    override fun nouvelleQuestion() {
+        TODO("Not yet implemented")
+    }
+
+    override fun repeteLaQuestion() {
+        TODO("Not yet implemented")
+    }
+
+    override fun donneMoiLeResultat(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun ecritLaQuestion(): String {
+        TODO("Not yet implemented")
+    }
+}
+
