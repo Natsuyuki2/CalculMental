3bff04899d26f95506023754232fd530498384b0
Lia <n@n>
2020-04-12 17:23:33 +0200

    Rework layout margins

--- app/src/main/java/com/example/calculmental/MainActivity.kt
+++ app/src/main/java/com/example/calculmental/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.calculmental
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.calculmental.model.TheVoice
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val theVoice = TheVoice(applicationContext)
+
+        val nouvelleQuestionBoutton : Button = findViewById(R.id.nouvelleQuestion_button)
+        nouvelleQuestionBoutton.setOnClickListener {
+            theVoice.nouvelleQuestion()
+        }
+        val répéteLaQuestion : Button = findViewById(R.id.répéter)
+        répéteLaQuestion.setOnClickListener {
+            theVoice.repeteLaQuestion()
+        }
     }
 }
--- app/src/main/java/com/example/calculmental/model/TheVoice.kt
+++ app/src/main/java/com/example/calculmental/model/TheVoice.kt
@@ -1,12 +1,51 @@
 package com.example.calculmental.model
 
-class TheVoice : CalculMental {
+import android.content.Context
+import android.media.MediaPlayer
+import com.example.calculmental.R
+import java.util.*
+
+class TheVoice (private val context : Context) : CalculMental {
+
     override fun nouvelleQuestion() {
-        TODO("Not yet implemented")
+
+        membreDeGauche = Random().nextInt(10) + 1
+
+        membreDeDroite = Random().nextInt(10) + 1
+
+        operation = Random().nextInt(2) + 1
+
+        idDesSonsALire = listOf(
+            listeDesNombres[membreDeGauche - 1],
+            listeDesOperation[operation - 1],
+            listeDesNombres[membreDeDroite - 1],
+            R.raw.lianah_egal)
+
+        caseALire = 0
+        lire()
     }
 
+    private fun lire() {
+
+        player = MediaPlayer.create(context, idDesSonsALire!![caseALire])
+
+        player?.setOnCompletionListener {
+            caseALire += 1
+
+            if (caseALire < idDesSonsALire!!.size) {
+                lire()
+            }
+        }
+
+        player?.start()
+
+    }
+
+
+
+
     override fun repeteLaQuestion() {
-        TODO("Not yet implemented")
+        lire()
     }
 
     override fun donneMoiLeResultat(): Int {
@@ -16,5 +55,28 @@ class TheVoice : CalculMental {
     override fun ecritLaQuestion(): String {
         TODO("Not yet implemented")
     }
-}
+    private val listeDesNombres = listOf(
+        R.raw.lianah_1,
+        R.raw.lianah_2,
+        R.raw.lianah_3,
+        R.raw.lianah_4,
+        R.raw.lianah_5,
+        R.raw.lianah_6,
+        R.raw.lianah_7,
+        R.raw.lianah_8,
+        R.raw.lianah_9,
+        R.raw.lianah_10)
 
+    private val listeDesOperation = listOf(
+        R.raw.lianah_plus,
+        R.raw.lianah_moins)
+
+    private var membreDeGauche : Int = 0
+    private var membreDeDroite : Int = 0
+    private var operation : Int = 0
+    private var player : MediaPlayer? = null
+
+    private var idDesSonsALire : List<Int>? = null
+
+    private var caseALire : Int = 0
+}
